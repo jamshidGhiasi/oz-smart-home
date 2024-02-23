462a3d83a5a7c97e3a7f15f7b2040e9e8e918471
Jamshid Ghiasinejad <jamshidghiasinejad@jamshidcstudio7.lan>
2024-02-23 16:09:02 +1100

    Fixing special characters

--- app/(pages)/services/page.tsx
+++ app/(pages)/services/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import OSHCopy from "@/components/shared/content/osh-copy";
 import ContentWrapper from "@/components/shared/layout/content-wrapper";
 import { Metadata } from "next";
@@ -18,28 +19,28 @@ const ServicesPage = async () => {
       </div>
       Smart Lighting:
 
-"Revolutionize your home's lighting with our Smart Lighting solutions. Effortlessly control and customize your lights to match your lifestyle, all while enhancing security and energy efficiency. Smart lighting not only provides convenience but also offers added layers of security by allowing you to remotely control your lights, deterring potential intruders. With energy-saving features, you can lower utility costs without compromising on comfort or safety."
-Smart Blinds and Curtains:
+      "Revolutionize your home's lighting with our Smart Lighting solutions. Effortlessly control and customize your lights to match your lifestyle, all while enhancing security and energy efficiency. Smart lighting not only provides convenience but also offers added layers of security by allowing you to remotely control your lights, deterring potential intruders. With energy-saving features, you can lower utility costs without compromising on comfort or safety."
+      Smart Blinds and Curtains:
 
-"Experience the convenience and efficiency of Smart Blinds and Curtains. These innovative solutions not only add a touch of luxury to your home but also enhance security and privacy. Easily manage natural light and privacy settings with the touch of a button or a voice command. Smart blinds and curtains contribute to energy efficiency by regulating sunlight, reducing the need for excessive air conditioning or heating. This intelligent system helps you maintain a comfortable environment while keeping costs low."
-Smart Air Conditioning:
+      "Experience the convenience and efficiency of Smart Blinds and Curtains. These innovative solutions not only add a touch of luxury to your home but also enhance security and privacy. Easily manage natural light and privacy settings with the touch of a button or a voice command. Smart blinds and curtains contribute to energy efficiency by regulating sunlight, reducing the need for excessive air conditioning or heating. This intelligent system helps you maintain a comfortable environment while keeping costs low."
+      Smart Air Conditioning:
 
-"Transform the way you cool your home with Smart Air Conditioning. Our systems are designed for optimal comfort, security, and energy efficiency. Smart AC adapts to your schedule and preferences, ensuring rooms are always at the perfect temperature when you need them. With remote access, you can adjust settings from anywhere, improving security by simulating occupancy. Enjoy cost savings with energy-efficient cooling, as the system intelligently adjusts based on usage patterns, reducing electricity bills without compromising comfort."
-Access and Intercom:
+      "Transform the way you cool your home with Smart Air Conditioning. Our systems are designed for optimal comfort, security, and energy efficiency. Smart AC adapts to your schedule and preferences, ensuring rooms are always at the perfect temperature when you need them. With remote access, you can adjust settings from anywhere, improving security by simulating occupancy. Enjoy cost savings with energy-efficient cooling, as the system intelligently adjusts based on usage patterns, reducing electricity bills without compromising comfort."
+      Access and Intercom:
 
-"Secure your home with Access and Intercom systems that prioritize safety, privacy, and convenience. Our solutions allow you to control and monitor access to your home, enhancing security while providing peace of mind. Smart locks and intercoms enable secure entry, even when you're away, minimizing the risk of unauthorized access. With encrypted communication, your privacy is safeguarded. These systems not only bolster security but also contribute to energy efficiency by eliminating the need for keys and reducing the likelihood of energy loss through unlocked doors."
-CCTV - Security & Alarm:
+      "Secure your home with Access and Intercom systems that prioritize safety, privacy, and convenience. Our solutions allow you to control and monitor access to your home, enhancing security while providing peace of mind. Smart locks and intercoms enable secure entry, even when you're away, minimizing the risk of unauthorized access. With encrypted communication, your privacy is safeguarded. These systems not only bolster security but also contribute to energy efficiency by eliminating the need for keys and reducing the likelihood of energy loss through unlocked doors."
+      CCTV - Security & Alarm:
 
-"Protect your home with our comprehensive CCTV - Security & Alarm systems. Our advanced cameras and alarms provide 24/7 surveillance, deterring potential intruders and enhancing security. CCTV footage can be accessed remotely, offering peace of mind and assisting authorities in case of incidents. With motion detection and smart alerts, you'll always be informed of any unusual activity. Our security systems not only prioritize your safety and privacy but also contribute to energy efficiency by providing a proactive approach to home security. All this is achieved with cost-effective solutions tailored to your needs."
-Home Entertainment:
+      "Protect your home with our comprehensive CCTV - Security & Alarm systems. Our advanced cameras and alarms provide 24/7 surveillance, deterring potential intruders and enhancing security. CCTV footage can be accessed remotely, offering peace of mind and assisting authorities in case of incidents. With motion detection and smart alerts, you'll always be informed of any unusual activity. Our security systems not only prioritize your safety and privacy but also contribute to energy efficiency by providing a proactive approach to home security. All this is achieved with cost-effective solutions tailored to your needs."
+      Home Entertainment:
 
-"Elevate your entertainment experience with our Home Entertainment solutions. Enjoy immersive audio and visual experiences in the comfort of your home while maintaining security, privacy, and energy efficiency. Our home theaters and audio systems are designed to deliver superior quality without compromising on safety. Integrated with smart controls, you can create the perfect ambiance for movie nights or entertaining guests. These systems are not only a source of enjoyment but also contribute to energy efficiency by optimizing power consumption and reducing standby usage, keeping costs low."
-Home Network:
+      "Elevate your entertainment experience with our Home Entertainment solutions. Enjoy immersive audio and visual experiences in the comfort of your home while maintaining security, privacy, and energy efficiency. Our home theaters and audio systems are designed to deliver superior quality without compromising on safety. Integrated with smart controls, you can create the perfect ambiance for movie nights or entertaining guests. These systems are not only a source of enjoyment but also contribute to energy efficiency by optimizing power consumption and reducing standby usage, keeping costs low."
+      Home Network:
 
-"Stay connected seamlessly with our Home Network solutions. Our robust and secure networks ensure fast, reliable connectivity for all your devices while prioritizing security and privacy. With encrypted connections and advanced firewall features, your data remains protected from cyber threats. Our networks are designed for energy efficiency, optimizing data transmission to reduce power consumption. Enjoy uninterrupted connectivity and peace of mind, knowing your network is reliable, secure, and cost-effective."
-Automation:
+      "Stay connected seamlessly with our Home Network solutions. Our robust and secure networks ensure fast, reliable connectivity for all your devices while prioritizing security and privacy. With encrypted connections and advanced firewall features, your data remains protected from cyber threats. Our networks are designed for energy efficiency, optimizing data transmission to reduce power consumption. Enjoy uninterrupted connectivity and peace of mind, knowing your network is reliable, secure, and cost-effective."
+      Automation:
 
-"Experience the convenience and efficiency of home automation with our tailored solutions. Our Automation systems streamline daily tasks, enhance security, and improve energy efficiency while keeping costs low. From scheduling lights and appliances to integrating with security systems, automation makes life simpler and safer. Smart automation not only provides convenience but also contributes to energy savings by optimizing usage patterns and reducing wastage. With our cost-effective solutions, you can enjoy a smarter, more efficient home without breaking the bank."
+      "Experience the convenience and efficiency of home automation with our tailored solutions. Our Automation systems streamline daily tasks, enhance security, and improve energy efficiency while keeping costs low. From scheduling lights and appliances to integrating with security systems, automation makes life simpler and safer. Smart automation not only provides convenience but also contributes to energy savings by optimizing usage patterns and reducing wastage. With our cost-effective solutions, you can enjoy a smarter, more efficient home without breaking the bank."
     </ContentWrapper>
   );
 }
