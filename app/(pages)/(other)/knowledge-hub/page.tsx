00410c1095fd3706f1a4cdec30ea4c87c8009f5a
Jamshid Ghiasinejad <jamshidghiasinejad@jamshids-mbp.lan>
2024-02-25 21:50:47 +1100

    Fix special charactr issue

--- app/(pages)/(other)/knowledge-hub/page.tsx
+++ app/(pages)/(other)/knowledge-hub/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unescaped-entities */
+
 import OSHCopy from "@/components/shared/content/osh-copy";
 import ContentWrapper from "@/components/shared/layout/content-wrapper";
 import { Metadata } from "next";
