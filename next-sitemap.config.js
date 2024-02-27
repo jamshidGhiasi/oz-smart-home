634316b4eb041fa4fe46f27c10be05022dd5bb55
Jamshid Ghiasinejad <jamshidghiasinejad@jamshidcstudio7.lan>
2024-02-27 12:07:25 +1100

    Added sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl: process.env.SITE_URL || 'https://ozsmarthome.com.au',
-    generateRobotsTxt: true, // (optional)
+    generateRobotsTxt: true,
+    generateRobotsTxt: {
+        additionalSitemaps: [
+            `${
+              process.env.SITE_URL || "https://ozsmarthome.com.au"
+            }/server-sitemap.xml`,
+          ],
+    }, // (optional)
+    
     // ...other options
   }
